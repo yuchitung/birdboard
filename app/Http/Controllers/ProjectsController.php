c00fb68ce948c2baec3c8fc9de2150222c37a161
Yuchi Tung <tracy770830@gmail.com>
2020-05-11 23:38:28 +0800

    refactor: store method on ProjectsController

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -20,14 +20,10 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        $attributes = request()->validate([
+        auth()->user()->projects()->create(request()->validate([
             'title' => 'required',
             'description' => 'required'
-        ]);
-
-        $attributes['owner_id'] = auth()->id();
-
-        Project::create($attributes);
+        ]));
 
         return redirect('/projects');
     }
