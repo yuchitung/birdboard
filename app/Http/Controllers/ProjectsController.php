f6c4e0bc41ec5d5a33111822f33f98d39ef2b911
Yuchi Tung <tracy770830@gmail.com>
2020-05-11 19:05:46 +0800

    feat: validate parameter 'description' on store method

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -15,7 +15,7 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        request()->validate(['title' => 'required']);
+        request()->validate(['title' => 'required', 'description' => 'required']);
         Project::create(request(['title', 'description']));
 
         return redirect('/projects');
