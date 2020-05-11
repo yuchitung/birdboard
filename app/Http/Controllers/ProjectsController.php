7d698a0c9c96a201f8d83d5f3394acba5db6b7c6
Yuchi Tung <tracy770830@gmail.com>
2020-05-11 22:14:26 +0800

    feat: validate owner_id

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -22,7 +22,8 @@ class ProjectsController extends Controller
     {
         $attributes = request()->validate([
             'title' => 'required',
-            'description' => 'required'
+            'description' => 'required',
+            'owner_id' => 'required'
         ]);
 
         Project::create($attributes);
