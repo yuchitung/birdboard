5ceca397e1c4d1f4b94f5d3651eeaa6628c3aa3b
Yuchi Tung <tracy770830@gmail.com>
2020-05-11 13:41:23 +0800

    feat: add url [GET]/projects

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/projects', function () {
+    $projects = App\Project::all();
+    return view('projects.index', compact('projects'));
+});
+
 Route::post('/projects', function () {
     App\Project::create(request(['title', 'description']));
 });
