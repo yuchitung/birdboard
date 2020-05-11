bfae98d6b4e22b6216e375db56de842af59ae1c7
Yuchi Tung <tracy770830@gmail.com>
2020-05-11 19:42:52 +0800

    refactor: ProjectsController

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -13,16 +13,18 @@ class ProjectsController extends Controller
         return view('projects.index', compact('projects'));
     }
 
-    public function show()
+    public function show(Project $project)
     {
-        $project = Project::findOrFail(request('project'));
-
         return view('projects.show', compact('project'));
     }
 
     public function store()
     {
-        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
         Project::create($attributes);
 
         return redirect('/projects');
