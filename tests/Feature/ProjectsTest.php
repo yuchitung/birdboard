b4b9ce7b9802e6af4578550f6d77bb181a278298
Yuchi Tung <tracy770830@gmail.com>
2020-05-12 14:34:05 +0800

    test: add test <an_authenticated_user_cannot_view_the_projects_of_others>

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -51,12 +51,21 @@ class ProjectsTest extends TestCase
     public function a_user_can_view_their_project()
     {
         $this->be(factory('App\User')->create());
-        $this->withoutExceptionHandling();
 
         $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->be(factory('App\User')->create());
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 
     /** @test */
