06ff78673b6fe2bcef4ed717a5041ec20beadf03
Yuchi Tung <tracy770830@gmail.com>
2020-05-14 18:25:26 +0800

    feat: create a new task form

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -13,13 +13,20 @@
 
     <main>
         <div class="lg:flex -mx-3">
-            <div class="lg:w-3/4 px-3 mb-8">
+            <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-6">
                     <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
                     {{-- tasks --}}
                     @foreach($project->tasks as $task)
                         <div class="card mb-3">{{$task->body}}</div>
                     @endforeach
+
+                    <div class="card mb-3">
+                        <form action="{{$project->path().'/tasks'}}" method="POST">
+                            @csrf
+                            <input placeholder="Add a new task..." class="w-full" name="body">
+                        </form>
+                    </div>
                 </div>
                 <div>
                     <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
