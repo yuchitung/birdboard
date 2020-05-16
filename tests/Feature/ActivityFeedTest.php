cc702b11dad66538d2d981483e62dd08ead755b2
Yuchi Tung <tracy770830@gmail.com>
2020-05-16 10:57:25 +0800

    feat: record an activity when project is created or updated

---
+++ tests/Feature/ActivityFeedTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Facades\Tests\Setup\ProjectFactory;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ActivityFeedTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function creating_a_project_records_activity()
+    {
+        $project = ProjectFactory::create();
+        $this->assertCount(1, $project->activity);
+        $this->assertEquals('created', $project->activity->first()->description);
+    }
+
+    /** @test */
+    function updating_a_project_records_activity()
+    {
+        $project = ProjectFactory::create();
+
+        $project->update(['title' => 'Changed']);
+
+        $this->assertCount(2, $project->activity);
+        $this->assertEquals('updated', $project->activity->last()->description);
+    }
+}
