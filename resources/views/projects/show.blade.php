cc9230a2f9a2015eadc8e33aec8b7932ec366c53
Yuchi Tung <tracy770830@gmail.com>
2020-05-20 12:54:28 +0800

    feat: buid a theme switcher

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,12 +1,13 @@
 @extends ('layouts.app')
 
 @section('content')
-    <header class="flex items-center mb-3 py-4">
+    <header class="flex items-center mb-6 pb-4">
         <div class="flex justify-between items-end w-full">
-            <p class="text-grey text-sm font-normal">
-                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a>
-                / {{$project->title}}
+            <p class="text-muted font-light">
+                <a href="/projects" class="text-muted no-underline hover:underline">My Projects</a>
+                / {{ $project->title }}
             </p>
+
             <div class="flex items-center">
                 @foreach ($project->members as $member)
                     <img
@@ -28,56 +29,57 @@
     <main>
         <div class="lg:flex -mx-3">
             <div class="lg:w-3/4 px-3 mb-6">
-                <div class="mb-6">
-                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                <div class="mb-8">
+                    <h2 class="text-lg text-muted font-light mb-3">Tasks</h2>
+
                     {{-- tasks --}}
-                    @foreach($project->tasks as $task)
+                    @foreach ($project->tasks as $task)
                         <div class="card mb-3">
-                            <form method="POST" action="{{$task->path()}}">
+                            <form method="POST" action="{{ $task->path() }}">
                                 @method('PATCH')
                                 @csrf
-                                <div class="flex">
-                                    <input name="body" value="{{$task->body}}"
-                                           class="w-full {{$task->completed?'text-grey':''}}">
-                                    <input type="checkbox" name="completed"
-                                           onchange="this.form.submit()"{{$task->completed? 'checked':''}}>
+
+                                <div class="flex items-center">
+                                    <input name="body" value="{{ $task->body }}" class="text-default bg-card w-full {{ $task->completed ? 'line-through text-muted' : '' }}">
+                                    <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
                             </form>
                         </div>
                     @endforeach
 
                     <div class="card mb-3">
-                        <form action="{{$project->path().'/tasks'}}" method="POST">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
                             @csrf
-                            <input placeholder="Add a new task..." class="w-full" name="body">
+
+                            <input placeholder="Add a new task..." class="text-default bg-card w-full" name="body">
                         </form>
                     </div>
                 </div>
+
                 <div>
-                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                    <h2 class="text-lg text-muted font-light mb-3">General Notes</h2>
+
                     {{-- general notes --}}
-                    <form method="POST" action="{{$project->path()}}">
+                    <form method="POST" action="{{ $project->path() }}">
                         @csrf
                         @method('PATCH')
+
                         <textarea
                             name="notes"
-                            class="card w-full mb-4"
+                            class="card text-default w-full mb-4"
                             style="min-height: 200px"
-                            placeholder="Anything special that you want to make a note of?">{{$project->notes}}</textarea>
+                            placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
+
                         <button type="submit" class="button">Save</button>
                     </form>
-                    @if ($errors->any())
-                        <div class="field mt-6">
-                            @foreach ($errors->all() as $error)
-                                <li class="text-sm text-red">{{ $error }}</li>
-                            @endforeach
-                        </div>
-                    @endif
+
+                    @include ('errors')
                 </div>
             </div>
 
-            <div class="lg:w-1/4 px-3">
-                @include('projects.card')
+            <div class="lg:w-1/4 px-3 lg:py-8">
+                @include ('projects.card')
                 @include ('projects.activity.card')
 
                 @can ('manage', $project)
